56a3e0e2a1441f11eb6a125facc6e920ce24f024
jmurillov1 <gatitojm.17@hotmail.com>
2019-05-22 21:06:14 -0500

    Practica03-Javascript

---
+++ 2.Validaciones/controladores/saludo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2> Bienvenido, pasaste las validaciones <?php echo $_POST["nombres"] ?></h2>
+</body>
+
+</html>
